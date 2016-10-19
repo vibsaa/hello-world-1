377d5b58d27ede94c56e55effcca645b98614144
vibsaa <rocketvibs@outlook.com>
2016-10-19 10:00:45 -0700

    better way of swapping without the third variable!

--- swap.c
+++ swap.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-int main()
-{
-	int x,y,temp;
-	printf("enter first number\n");
-	scanf("%d",&x);
-	printf("\nenter second number\n");
-	scanf("%d",&y);
-	temp=x;
-	x=y;
-	y=temp;
-	printf("\nswapped numbers are %d and %d\n",x,y);
-	return 0;
-}
+    #include <stdio.h>
+    void main()
+    {
+    int a,b;
+    printf("enter first number");
+    scanf("%d",a);
+    printf("enter second number ");
+    scanf("%d",b);
+    printf("value of a and b before swapping  is a=%d,b=%d"a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("value of a and b after swapping  is a=%d,b=%d"a,b);
+    }
